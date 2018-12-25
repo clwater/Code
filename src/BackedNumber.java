616c815321755e73f0237c63c036c6263838ff5f
1 <email>
2018-12-25 17:27:48 +0800

    Code BackedNumber

---
+++ src/BackedNumber.java
@@ -0,0 +1,42 @@
+/**
+ *给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+ * 示例 1:
+ * 输入: 123
+ * 输出: 321
+ *  示例 2:
+ * 输入: -123
+ * 输出: -321
+ * 示例 3:
+ * 输入: 120
+ * 输出: 21
+ * 注意:
+ * 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−231,  231 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
+ */
+public class BackedNumber {
+    public static void main(String[] args){
+        System.out.println(new BackedNumber_Solution().isPalindrome(12344321));
+        System.out.println(new BackedNumber_Solution().isPalindrome(-10));
+        System.out.println(new BackedNumber_Solution().isPalindrome(123421));
+        System.out.println(new BackedNumber_Solution().isPalindrome(12321));
+    }
+}
+
+
+class BackedNumber_Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (( x % 10 == 0) && x != 0)){
+            return false;
+        }
+
+        int rev = 0;
+
+        while (x > rev){
+            rev = rev * 10 + x % 10;
+            x = x / 10;
+        }
+
+        return x == rev || x == rev /10;
+
+    }
+}
+
