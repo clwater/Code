dccff46a9700fbd5fbca75fff7d887449906f435
clwater <gengzhibo@gengzhibo.net>
2019-01-08 22:01:02 +0800

    20180108

---
+++ src/undo/LargestRectangleInHistogram.java
@@ -0,0 +1,35 @@
+package undo;
+
+/**
+ *给定 n 个非负整数，用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为 1 。
+ * 求在该柱状图中，能够勾勒出来的矩形的最大面积。
+ * 以上是柱状图的示例，其中每个柱子的宽度为 1，给定的高度为 [2,1,5,6,2,3]。
+ * 图中阴影部分为所能勾勒出的最大矩形面积，其面积为 10 个单位。
+ *
+ * 示例:
+ *
+ * 输入: [2,1,5,6,2,3]
+ * 输出: 10
+ */
+public class LargestRectangleInHistogram {
+    public static void main(String[] args) {
+
+    }
+}
+
+class LargestRectangleInHistogram_Solution {
+    public int largestRectangleArea(int[] heights) {
+        int min = heights[0];
+        for(int index: heights){
+            min = Math.min(index, min);
+        }
+        int result = heights.length * min;
+
+        int l = 0, r = heights.length - 1;
+        while (l <= r){
+
+        }
+
+        return 0;
+    }
+}
