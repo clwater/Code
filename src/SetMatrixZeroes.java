59c370f8b9cb2cd538a17ac7f6bbf5abcfe9b1b8
clwater <gengzhibo@gengzhibo.net>
2019-08-13 21:37:21 +0800

    code: 73矩阵置零2

--- src/SetMatrixZeroes.java
+++ src/SetMatrixZeroes.java
@@ -22,37 +22,70 @@ import java.util.Set;
 
 public class SetMatrixZeroes {
     public static void main(String[] args) {
+//        int[][] matrix = new int[][]{
+//                {0, 1, 2, 0},
+//                {3, 4, 5, 2},
+//                {1, 3, 1, 5}
+//        };
+
         int[][] matrix = new int[][]{
-                {0, 1, 2, 0},
-                {3, 4, 5, 2},
-                {1, 3, 1, 5}
+                {0, 1},
         };
         new SetMatrixZeroes_Solution().setZeroes(matrix);
     }
 }
 
 class SetMatrixZeroes_Solution {
+
     public void setZeroes(int[][] matrix) {
-        Set<Integer> row = new HashSet<>();
-        Set<Integer> col = new HashSet<>();
-
+        boolean firstCol = false, firstRow = false;
 
         for (int i = 0 ; i < matrix.length; i++){
-            for (int j = 0; j < matrix[i].length; j++){
+            if (matrix[i][0] == 0){
+                firstCol = true;
+                break;
+            }
+        }
+
+        for (int i = 0 ; i < matrix[0].length; i++){
+            if (matrix[0][i] == 0){
+                firstRow = true;
+                break;
+            }
+        }
+
+        for (int i = 0 ; i < matrix.length; i++){
+            for (int j = 0; j < matrix[i].length; j++) {
                 if (matrix[i][j] == 0){
-                    row.add(i);
-                    col.add(j);
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
 
-        for (int i = 0 ; i < matrix.length; i++){
-            for (int j = 0; j < matrix[i].length; j++){
-                if (row.contains(i) || col.contains(j)){
-                    matrix[i][j] = 0;
+        if ( matrix.length > 1 && matrix[0].length > 1){
+            for (int i = 1 ; i < matrix.length; i++){
+                for (int j = 1; j < matrix[i].length; j++) {
+                    if (matrix[i][0] == 0 || matrix[0][j] == 0){
+                        matrix[i][j] = 0;
+                    }
                 }
             }
         }
+
+        if (firstCol){
+            for (int i = 0 ; i < matrix.length; i++){
+                matrix[i][0] = 0;
+            }
+        }
+
+        if (firstRow){
+            for (int i = 0 ; i < matrix[0].length; i++){
+                matrix[0][i] = 0;
+            }
+        }
+
+
 
         for (int i = 0 ; i < matrix.length; i++){
             for (int j = 0; j < matrix[i].length; j++){
@@ -61,6 +94,36 @@ class SetMatrixZeroes_Solution {
             System.out.println();
         }
     }
+
+//    public void setZeroes(int[][] matrix) {
+//        Set<Integer> row = new HashSet<>();
+//        Set<Integer> col = new HashSet<>();
+//
+//
+//        for (int i = 0 ; i < matrix.length; i++){
+//            for (int j = 0; j < matrix[i].length; j++){
+//                if (matrix[i][j] == 0){
+//                    row.add(i);
+//                    col.add(j);
+//                }
+//            }
+//        }
+//
+//        for (int i = 0 ; i < matrix.length; i++){
+//            for (int j = 0; j < matrix[i].length; j++){
+//                if (row.contains(i) || col.contains(j)){
+//                    matrix[i][j] = 0;
+//                }
+//            }
+//        }
+//
+//        for (int i = 0 ; i < matrix.length; i++){
+//            for (int j = 0; j < matrix[i].length; j++){
+//                System.out.print(matrix[i][j] + ", ");
+//            }
+//            System.out.println();
+//        }
+//    }
 
 
 }
